b3b021a0cd074dc95c48cbc8ad0639f110d879a5
Nurlan Shukurov <shukurov.nurlan.1991@gmail.com>
2025-01-23 13:03:41 +0400

    Added data classes

---
+++ app/src/main/java/com/vagonn/tmdbclient/data/Artist.kt
@@ -0,0 +1,14 @@
+package com.vagonn.tmdbclient.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Artist(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("popularity")
+    val popularity: Double?,
+    @SerializedName("profile_path")
+    val profilePath: String?
+)
---
+++ app/src/main/java/com/vagonn/tmdbclient/data/TVShow.kt
@@ -0,0 +1,16 @@
+package com.vagonn.tmdbclient.data
+
+import com.google.gson.annotations.SerializedName
+
+data class TVShow(
+    @SerializedName("first_air_date")
+    val firstAirDate: String?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("overview")
+    val overview: String?,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+)
